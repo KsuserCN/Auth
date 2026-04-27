9dba96102ba5854ec7fcbb3e80d97f89a8e35203
Ksuserkqy <2765301200@qq.com>
2026-04-27 10:12:28 +0800

    new: 新增ios demo

---
+++ mobile/ios/Ksuser Auth/Ksuser_AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ksuser_AuthApp.swift
+//  Ksuser Auth
+//
+//  Created by Ksuserkqy on 2026/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Ksuser_AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
